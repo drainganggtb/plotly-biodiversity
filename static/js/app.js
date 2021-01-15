563e10342f9777c4f4fc75a6cca555c7498ff904
Justin <gree2167@umn.edu>
2021-01-14 18:33:55 -0600

    figuring out how to get data to show up

--- static/js/app.js
+++ static/js/app.js
@@ -1,8 +1,1 @@
-function plotbuilder (otu){
-    var selector = d3.select("#selDataset");
-    //use d3 to read JSON file
-    d3.json("samples.json").then((importedData) => {
-        console.log(importedData)
-    });
-}
 
